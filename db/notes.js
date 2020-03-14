770cf084bea15505ff2be665232368b1b5eee606
jenniferkirwin <lamontagneartist@gmail.com>
2020-03-14 18:18:16 -0400

    Rendered saved notes

---
+++ db/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+    {
+        routeName: "notetest",
+        title: "My First Note",
+        content: "Lots of lovely text",
+        favorite: true
+    },
+    {
+        routeName: "notetest2",
+        title: "My Second Note",
+        content: "Lots of lovely text",
+        favorite: true
+    },
+    {
+        routeName: "notetest3",
+        title: "My Third Note",
+        content: "Lots of lovely text",
+        favorite: false
+    }
+];
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -11,12 +11,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`public`));
 app.use(express.json());
 
+const notes = require(`./db/notes`);
+
+// const notes = [
+//     {
+//         routeName: "notetest",
+//         title: "My First Note",
+//         content: "Lots of lovely text",
+//         favorite: true
+//     },
+//     {
+//         routeName: "notetest2",
+//         title: "My Second Note",
+//         content: "Lots of lovely text",
+//         favorite: true
+//     }
+// ];
+
 // -------------------------------------------------------------------
 // Routes
 // -------------------------------------------------------------------
 
 app.get(`/`, function(reg, res){
     res.sendFile(path.join(__dirname, `../public/index.html`));
+});
+
+app.get(`/api/notes`, function(reg, res){
+    return res.json(notes);
 });
 
 // app.get(`/`, function(reg, res){
